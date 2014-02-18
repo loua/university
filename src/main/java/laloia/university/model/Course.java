db1b3b07245d62038481360d3c35b1b125f14f44
lou <lou.aloia@gmail.com>
2014-02-17 22:23:14 -0600

    Initial commit.

---
+++ src/main/java/laloia/university/model/Course.java
@@ -0,0 +1,73 @@
+package laloia.university.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Entity implementation class for Entity: Course
+ * 
+ */
+@Entity
+public class Course {
+
+	private int id;
+	private String number;
+	private String description;
+	private String name;
+	private int creditHours;
+	private Department department;
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public Course() {
+		super();
+	}
+
+	@Id
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNumber() {
+		return this.number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCreditHours() {
+		return this.creditHours;
+	}
+
+	public void setCreditHours(int creditHours) {
+		this.creditHours = creditHours;
+	}
+
+}
---
+++ src/main/java/laloia/university/model/Department.java
@@ -0,0 +1,38 @@
+package laloia.university.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Department {
+
+	private int id;
+	private String name;
+	private String code;
+
+	@Id
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+}
