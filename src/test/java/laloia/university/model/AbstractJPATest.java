ea8d2cc4b36175b7b450cd7b2244a33675fb8dc1
lou <lou.aloia@gmail.com>
2014-03-07 21:30:25 -0600

    Refactored test class

---
+++ src/test/java/laloia/university/model/AbstractJPATest.java
@@ -0,0 +1,57 @@
+package laloia.university.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+public abstract class AbstractJPATest {
+	private static String persistenceUnit = "university-model";
+	private static EntityManagerFactory emf;
+	
+	EntityManager em;
+
+	@BeforeClass
+	public static void setUpOnce() throws Exception {
+		emf = Persistence.createEntityManagerFactory(persistenceUnit);
+	}
+
+	@AfterClass
+	public static void tearDownOnce() {
+		emf.close();
+	}
+
+	@Before
+	public void setUpBase() throws Exception {
+		em = emf.createEntityManager();
+	}
+
+	@After
+	public void tearDownBase() throws Exception {
+		em.close();
+	}
+
+	IDatabaseConnection getDbConnectionFromDriver() throws Exception {
+		Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+		return new DatabaseConnection(DriverManager.getConnection(
+				"jdbc:derby:memory:university;create=true", "app", "password"));
+	}
+
+	IDatabaseConnection getDbConnection() throws Exception {
+		return new DatabaseConnection(getCurrentConnection());
+	}
+
+	Connection getCurrentConnection() {
+		return em.unwrap(java.sql.Connection.class);
+	}
+
+}
